d9c16d534556a59814dbd5a93e94bab7ecaba653
Zeneak <Joshua.Zimmerschied@gmail.com>
2022-03-23 14:48:17 -0700

    Added a practice space in section one project.

---
+++ section1/src/Practice/arrays.js
@@ -0,0 +1,17 @@
+const arrayT = [1,2,3,4,5]
+
+const total = 
+arrayT.push(15)
+
+console.log(arrayT.length) //expected 6
+console.log(arrayT[5]) //expected 15
+
+let a = ' plus '
+let b = ' equals '
+
+console.log(arrayT[0]+a+arrayT[1]+a+arrayT[2]+a+arrayT[3]+a+arrayT[4]+b+arrayT[5])
+
+// I want to make a string that for loops arrayT into a string.
+// expected 1 plus 2 plus 3 plus 4 plus 5 is 15
+
+//Created outcome desired, but code is basic and verbose. 
